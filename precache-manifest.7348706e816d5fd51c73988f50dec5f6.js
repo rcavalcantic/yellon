e8e1bb5954e50bfb6665e9f4d32c5e02f5aae429
Roberto <roberto.campos@scudra.com>
2020-03-18 18:52:04 -0400

    Updates

---
+++ precache-manifest.7348706e816d5fd51c73988f50dec5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf33f90ffbdeb7708a3d6647463e1026",
+    "url": "/yellon/index.html"
+  },
+  {
+    "revision": "eac7a1d4a5ef1fffcb3d",
+    "url": "/yellon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e3590029cd1a5e49fae7",
+    "url": "/yellon/static/js/2.235ac071.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/yellon/static/js/2.235ac071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac7a1d4a5ef1fffcb3d",
+    "url": "/yellon/static/js/main.95eeaef5.chunk.js"
+  },
+  {
+    "revision": "8dc2f3d1897ea5a6ce52",
+    "url": "/yellon/static/js/runtime-main.2aefe847.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yellon/static/media/logo.5d5d9eef.svg"
+  }
+]);

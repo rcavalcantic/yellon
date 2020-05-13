360cc9f8e1e90f8062ae38ed28cf21c2ca3c91e0
Roberto <roberto.temp@scudra.com>
2020-05-12 20:52:38 -0400

    refactor: update home at all

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faWindowClose } from '@fortawesome/free-solid-svg-icons'
-import { Header, HeaderWrapper, Logo, MenuButton, Sidebar, LogoSidebar } from './styles' 
+import { Divider, Header, HeaderWrapper, Logo, MenuButton, SentenceImg, Sidebar, LogoSidebar } from './styles' 
 import yellonLogo from '../../static/images/yellon-white.png'
+import sentence from '../../static/images/sejaon.png'
+import yellonOutline from '../../static/images/yellow-outline.png'
+import instagramIcon from '../../static/images/icone-insta.png'
+import facebookIcon from '../../static/images/icone-face.png'
 
 export default function Hero() {
   const [state, setState] = useState({
@@ -17,15 +21,23 @@ export default function Hero() {
     <Header>
       <HeaderWrapper onClick={() => {window.history.go("https://yellonpublicidade.com.br")}}>
         <Logo src={yellonLogo} alt="Yellon Publicidade logo" />
+        <Divider />
+        <SentenceImg src={sentence} alt="Yellon Publicidade logo" />
       </HeaderWrapper>
       { state.isMenuOpen && 
         <Sidebar>
-          <LogoSidebar src={yellonLogo} alt="menu lateral seja on"/>
-          <li style={{ display: "grid", justifyContent: "start", lineHeight: 0, fontWeight: "bold" }}>
-            <ul>Início</ul>
-            <ul>Portifólio</ul>
-            <ul>Equipe</ul>
+          <LogoSidebar src={yellonOutline} alt="menu lateral seja on"/>
+          <li style={{ display: "grid", justifyContent: "start", lineHeight: 0 }}>
+            <ul style={{ fontSize: '36px', color: 'black' }}>Home</ul>
+            <ul style={{ fontSize: '36px', color: 'black' }}>A Agência</ul>
+            <ul style={{ fontSize: '36px', color: 'black' }}>Portifólio</ul>
+            <ul style={{ fontSize: '36px', color: 'black' }}>Team Yellon</ul>
+            <ul style={{ fontSize: '36px', color: 'black' }}>Contatos</ul>
           </li>
+          <div style={{ margin: '32px'}}>
+            <img style={{ width: '36px'}} src={instagramIcon} alt={"Instagram icone"}/>
+            <img style={{ width: '36px', marginLeft: '-10px'}} src={facebookIcon} alt={"Facebook icone"}/>
+          </div>
         </Sidebar>
       }
       <MenuButton onClick={handleMenu}>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,26 +1,40 @@
 import styled from 'styled-components'
 import { tablet } from '../../utils/_breakpoints'
+import { yellow } from '../../utils/_colors'
 
 const Header = styled.header`
   position: fixed;
   display: flex;
   top: 0;
   width: 100%;
-  height: 10%;
+  height: 70px;
   background-color: black;
 `
 
 const HeaderWrapper = styled.div`
   margin: 0;
-  @media (min-width: ${tablet}) {
-    margin: 32px;
-  }
+`
+
+const Divider = styled.div`
+  border-left: 2px solid ${yellow};
+  display: inline-block;
+  width: 0;
+  height: 45px;
+  vertical-align: top;
+  margin-top: 13px;
+  margin-left: 20px;
+  margin-right: 20px;
+`
+
+const SentenceImg = styled.img`
+  margin-top: -10px;
+  width: 120px;
+  vertical-align: super;
 `
 
 const Logo = styled.img`
   max-height: 120px;
   max-width: 120px;
-  margin-top: -25px;
   margin-left: 8px;
   
   @media (min-width: ${tablet}) {
@@ -45,7 +59,7 @@ const MenuButton = styled.button`
   @media (min-width: ${tablet}) {
     padding: 8px;
     float: right;
-    margin-right: 256px;
+    margin-right: 17%;
     margin-top: 22px;
     border: solid 0.5px;
     
@@ -57,28 +71,29 @@ const MenuButton = styled.button`
 
 const Sidebar = styled.div`
   height: 100%;
-  background-color: black;
+  background-color: ${yellow};
   position: fixed;
   width: 50%;
-  transition: width 2s;
   z-index: -999;
-  background-color: black;
   right: 0;
-
+  transition: width 2s;
+  
   @media (min-width: ${tablet}) {
+    transition: width 2s;
     height: 100%;
-    width: 15%;
+    width: 22%;
+    margin-top: 64px;
   }
 `
 
 const LogoSidebar = styled.img`
   width: 120px;
-  height: 120px;
   padding: 36px 36px 0;
   
   @media (min-width: ${tablet}) {
     padding: 81px 36px 0;
+    margin-bottom: 120px;
   }
 `
 
-export { Header, HeaderWrapper, Logo, MenuButton, Sidebar, LogoSidebar }
+export { Divider, Header, HeaderWrapper, Logo, MenuButton, SentenceImg, Sidebar, LogoSidebar }
